eb7d010abb733a3cf8b3f32348ab4812408fb637
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-05-24 17:49:11 +0200

    Pushed previous refactoring further

--- src/graphic/chess_board.rs
+++ src/graphic/chess_board.rs
@@ -3,6 +3,48 @@ use gtk::prelude::*;
 use gtk::{Inhibit};
 use relm_derive::{Msg, widget};
 
+struct ChessBoardPainter {
+    
+}
+
+impl ChessBoardPainter {
+    fn draw_background(context: &cairo::Context, background_color: (f64, f64, f64)) {
+        let (bg_red, bg_green, bg_blue) = background_color;
+
+        context.set_source_rgb(bg_red, bg_green, bg_blue);
+        context.paint();
+    }
+
+    fn draw_cells(
+        context: &cairo::Context,
+        white_cells_color: (f64, f64, f64),
+        black_cells_color: (f64, f64, f64),
+        size: i32,
+    ) {
+        let (w_cells_red, w_cells_green, w_cells_blue) = white_cells_color;
+        let (b_cells_red, b_cells_green, b_cells_blue) = black_cells_color;
+
+        let cells_size = (size as f64) / 9.0;
+
+        for row in 0..8 {
+            for col in 0..8 {
+                let is_white_cell = (row+col) % 2 == 0;
+                if is_white_cell {
+                    context.set_source_rgb(w_cells_red, w_cells_green, w_cells_blue);
+                }
+                else {
+                    context.set_source_rgb(b_cells_red, b_cells_green, b_cells_blue);
+                }
+
+                let cell_x = cells_size * (0.5 + (col as f64));
+                let cell_y = cells_size * (0.5 + (row as f64));
+                context.rectangle(cell_x, cell_y, cells_size, cells_size);
+                context.fill();
+            }
+        }
+    }
+}
+
 #[allow(dead_code)]
 pub struct ChessBoardModel {
     size: i32,
@@ -77,39 +119,15 @@ impl Widget for ChessBoard {
     fn init_view(&mut self) {
         self.canvas.set_size_request(self.model.size, self.model.size);
 
-        let (bg_red, bg_green, bg_blue) = self.model.background_color;
-        let (w_cells_red, w_cells_green, w_cells_blue) = self.model.white_cells_color;
-        let (b_cells_red, b_cells_green, b_cells_blue) = self.model.black_cells_color;
 
-        let cells_size = (self.model.size as f64) / 9.0;
+        let background_color = self.model.background_color;
+        let white_cells_color = self.model.white_cells_color;
+        let black_cells_color = self.model.black_cells_color;
+        let size = self.model.size;
 
-        self.canvas.connect_draw(move |_src, context| {
-            let draw_background = || {
-                context.set_source_rgb(bg_red, bg_green, bg_blue);
-                context.paint();
-            };
-
-            let draw_cells = || {
-                for row in 0..8 {
-                    for col in 0..8 {
-                        let is_white_cell = (row+col) % 2 == 0;
-                        if is_white_cell {
-                            context.set_source_rgb(w_cells_red, w_cells_green, w_cells_blue);
-                        }
-                        else {
-                            context.set_source_rgb(b_cells_red, b_cells_green, b_cells_blue);
-                        }
-    
-                        let cell_x = cells_size * (0.5 + (col as f64));
-                        let cell_y = cells_size * (0.5 + (row as f64));
-                        context.rectangle(cell_x, cell_y, cells_size, cells_size);
-                        context.fill();
-                    }
-                }
-            };
-
-            draw_background();
-            draw_cells();
+        self.canvas.connect_draw(move |_source, context| {
+            ChessBoardPainter::draw_background(context, background_color);
+            ChessBoardPainter::draw_cells(context, white_cells_color, black_cells_color, size);
 
             Inhibit(true)
         });
